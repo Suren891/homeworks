0ba62a0443fba9273f4c94c408fa8194847d3cd9
Suren891 <100084537+Suren891@users.noreply.github.com>
2022-02-27 14:59:22 +0300

    Add files via upload

---
+++ Homework 1 27.02.22.js
@@ -0,0 +1,94 @@
+// varjutyun 1
+// let a = 14;
+// let b = 5
+// console.log (a + b);
+// console.log (a - b);
+// console.log (a * b);
+// console.log (a / b);
+// console.log (a % b);
+
+// let a = 99;
+// let b = 11;
+// console.log (a + b);
+// console.log (a - b);
+// console.log (a * b);
+// console.log (a / b);
+// console.log (a % b);
+
+// let a = 81;
+// let b = 16;
+// console.log (a + b);
+// console.log (a - b);
+// console.log (a * b);
+// console.log (a / b);
+// console.log (a % b);
+
+// varjutyun 2
+// const name1 = 'Suren';
+// const age = 45;
+// console.log (`My name is ${name1}, a am ${age}.`);
+
+//  varjutyun 3
+// let num = +prompt('Insert a number')
+// alert(num % 10);
+
+//  varjutyun 4
+// let num = +prompt (`Insert a number`);
+// if (num < 0) {
+//     alert ('Yes')
+// } else {
+//     alert ('No')
+// }
+
+//  varjutyun 5
+// let num1 = +prompt (`Insert a number`);
+// let num2 = +prompt (`Insert a number`);
+// if (num1 % num2 === 0 || num2 % num1 === 0) {
+//     alert ('1') 
+// }else{
+//     alert('0')
+//     }
+
+//  varjutyun 6
+// let num = +prompt ('Insert a number');
+// let month;
+// switch (num) {
+//     case 1: alert ('January');
+//         break;
+//     case 2: alert ('February');
+//         break;
+//     case 3: alert ('March');
+//         break;
+//     case 4: alert ('April');
+//         break;
+//     case 5: alert ('May');
+//         break;
+//     case 6: alert ('June');
+//         break;
+//     case 7: alert ('July');
+//         break;
+//     case 8: alert ('August');
+//         break;
+//     case 9: alert ('September');
+//         break;
+//     case 10: alert ('October');
+//         break;
+//     case 11: alert ('November');
+//         break;
+//     case 12: alert ('December');
+//         break;
+//     default:
+//         break;
+// }
+
+// varjutyun 7
+let a = +prompt (`Insert a number`);
+let b = +prompt (`Insert a number`);
+let c = +prompt (`Insert a number`);
+if (a > b && a > c) {
+    alert (`${a} is a maximum`)
+} if (b > a && b > c) {
+    alert (`${b} is a maximum`)
+} if (c > a && c > b) {
+    alert (`${c} is a maximum`)
+}
